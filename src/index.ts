187487d75639c6c3c506e8ea4d1448f37daf63e2
vracic <vracictin@gmail.com>
2023-10-20 18:32:57 +0200

    ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 const app = express();
+const port = 3001;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
 
